962675eb75108652cb702c524d46ebc0da7b47e5
Thotyelwa <mpongwanathotyelwa@gmail.com>
2023-07-31 15:46:07 +0200

    done

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const fs = require('fs');
 
 
-fs.writeFile('data.txt', 'Well Done!', (err) => {
-  if (err) {
-    console.error('Error writing the file:', err);
-  } else {
-    console.log('Well Done!');
-  
-    fs.writeFile('data.txt', '\nYou are the best!', { flag: 'a+' }, (err) => {
-      if (err) {
-        console.error('Error writing the file:', err);
-      } else {
-        console.log('You are the best!');
+fs.writeFileSync('./data.txt', 'Well Done!');
+ fs.writeFileSync('./data.txt', '\nYou are the best!')
        
-        fs.appendFile('data.txt', '\nYou are superb!', (err) => {
-          if (err) {
-            console.error('Error appending the file:', err);
-          } else {
-            console.log('You are superb!');
-          }
-        });
-      }
-    });
-  }
-});
+fs.appendFileSync('./data.txt', 'You are superb!')
+        
 
 
 const Person = require('./person');
